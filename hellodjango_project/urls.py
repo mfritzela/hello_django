1dc4ceaa2ac1cc1e0f3021fcb2ca8227e9a890eb
mfritzela <maria.fritzela@netcompany-intrasoft.com>
2022-11-03 13:13:20 +0200

    redirect root URL of site to myapi homepage

--- hellodjango_project/urls.py
+++ hellodjango_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapi/', include("myapi.urls")),
+]
+
+# redirect the root URL of the site to 'myapi' homepage
+urlpatterns += [
+    path('', RedirectView.as_view(url='myapi/', permanent=True)),
 ]
