7bad3a8a8cc70d3c18c750e9603065d5f753f722
mfritzela <maria.fritzela@netcompany-intrasoft.com>
2022-11-18 16:24:49 +0200

    Add get statistics of readings of sensor id = {id}

--- myapi/views.py
+++ myapi/views.py
@@ -6,6 +6,9 @@ from .serializers import SensorSerializer, ReadingSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Avg, Max, Min, IntegerField, FloatField, CharField
+from django.db.models.functions import Cast
+from django.db.models.fields.json import KeyTextTransform
 
 # Create your views here.
 
@@ -45,6 +48,33 @@ class SensorViewSet(ModelViewSet):
         serializer = ReadingSerializer(query, many=True)
         return Response(serializer.data)
 
+    # sensors/{id}/readings/stats GET statistics of readings of sensor id = {id}
+    @action(detail=True, url_path='readings/stats', url_name='readings-stats')
+    def sensor_readings_stats(self, request, pk=None):
+        query = SensorReading.objects.filter(sensorId=pk)
+        # check type of first reading to decide if stats can be calculated
+        # cast reading fields to appropriate types and filter by type (to avoid invalid types)
+        if query[0].reading["type"] == "int":
+            query = query.annotate(
+                reading_value = Cast(KeyTextTransform('value', 'reading'), output_field=IntegerField()),
+                reading_type = Cast(KeyTextTransform('type', 'reading'), output_field=CharField())
+            ).filter(reading_type= "int")
+        elif query[0].reading["type"] == "float":
+            
+            query = query.annotate(
+                reading_value = Cast(KeyTextTransform('value', 'reading'), output_field=FloatField()),
+                reading_type = Cast(KeyTextTransform('type', 'reading'), output_field=CharField())
+            ).filter(reading_type= "float")
+        else:
+            return Response()
+        query = query.aggregate(Avg('reading_value'), 
+                                Min('reading_value'), 
+                                Max('reading_value'))
+            
+        return Response(query.items())
+        
+
+
     # sensors/type/{type}/readings GET readings of sensors of type = {type}
     @action(detail=False, url_path=r'type/(?P<type_name>\w+)/readings', url_name='readings-type')
     def sensor_readings_type(self, request, type_name):
@@ -71,8 +101,8 @@ class ReadingViewSet(ModelViewSet):
         - timestamp returns readings created at the specific datetime
         input as  using the YYYY-MM-DD HH:MM Format. (time can be ommited)
         Examples:
-        # myapi/readings/?timestamp=2022-11-09
-        # myapi/readings/?timestamp=2022-11-08T16:00:00
+        - myapi/readings/?timestamp=2022-11-09
+        - myapi/readings/?timestamp=2022-11-08T16:00:00
         """
         queryset = super(ReadingViewSet, self).get_queryset()
 
