e92e87dbb89d7d1acf1cc016b0702974c6acf3bc
mfritzela <maria.fritzela@netcompany-intrasoft.com>
2022-11-30 14:54:41 +0200

    Use db backend Mongo running on localhost :djongo

--- hellodjango_project/settings.py
+++ hellodjango_project/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'hellodjango_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'test1',
     }
 }
 
